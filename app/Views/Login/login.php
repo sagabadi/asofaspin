709ead8f09ad815d2ffc05030f9c0dbd3f601c27
Bagas Adi Prayitno <aprayitno084@gmail.com>
2022-09-22 20:15:49 +0700

    Update login.php

--- app/Views/Login/login.php
+++ app/Views/Login/login.php
@@ -140,6 +140,16 @@
             if(data_valuable.includes(i.value)){
               console.log("N");
               spin();
+              $.ajax({
+                  url:"<?php echo base_url('/add_counter?keygen=20220922131233');?>",
+                  method:"POST",
+                  data:{id:id},
+                  dataType:"JSON",
+                  success:function(data)
+                  {
+
+                  }
+              });
             } else {
               setTimeout(function(){
                 // const waMessage = 'https://wa.me/6285697984834/?text=Halo!%0ASaya%20{nama}%20mau%20klaim%20hadiah%20{namahadiah}.%20Alamat:%20%20{alamat}%20.%20No%20HP:%20{nohp}.%20Terimakasih!';
@@ -171,16 +181,6 @@
       let resultValue = 101;
 
       function spin(){
-        $.ajax({
-            url:"<?php echo base_url('/add_counter?keygen=20220922131233');?>",
-            method:"POST",
-            data:{id:id},
-            dataType:"JSON",
-            success:function(data)
-            {
-
-            }
-        });
         //Start spinning
         spinBtn.disabled = true;
         //Empty final value
