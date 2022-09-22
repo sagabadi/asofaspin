9c310ae0d6d82b25f2e8ca19cfae2e928071bd62
Bagas Adi Prayitno <aprayitno084@gmail.com>
2022-09-22 19:36:09 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -40,7 +40,7 @@ $routes->get('/dashboard', 'Dashboard::index', ['filter' => 'auth']);
 $routes->get('/link', 'Link::index', ['filter' => 'auth']);
 $routes->post('/generate', 'Link::generate_link', ['filter' => 'auth']);
 $routes->post('/update_is_copy', 'Link::edit_is_copy', ['filter' => 'auth']);
-$routes->get('/update_is_claim', 'Link::edit_is_claim', ['filter' => 'auth']);
+$routes->get('/update_is_claim', 'Link::edit_is_claim');
 $routes->post('/store_hadiah', 'Dashboard::add_hadiah', ['filter' => 'auth']);
 $routes->post('/update_hadiah', 'Dashboard::edit_hadiah', ['filter' => 'auth']);
 $routes->post('/delete_hadiah', 'Dashboard::delete_hadiah', ['filter' => 'auth']);
